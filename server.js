e3f4fc7138069a87cb60ea70c67df40004762cc0
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-12-05 14:02:39 +0600

    add proxy in package.json under client-app to connect server site port

--- server.js
+++ server.js
@@ -3,8 +3,28 @@ const app = express();
 const http = require('http');
 const expressServer = http.createServer(app); //Create Express Server
 
+// ---------> ConnectedConfigure Socket.io with express server
+const {Server} = require('socket.io');
+const io = new Server(expressServer);
 
-expressServer.listen(3000, function(){
-    console.log("@Server run @ 3000!")
+
+app.get("/express-server", function(req, res){
+    res.end("This is from Backend!")
+})
+
+
+
+// ---------> User connection and disconnection status
+io.on('connection', function(socket){
+    console.log("New User Connected!")
+
+    socket.on('disconnect', function (){
+        console.log("User Disconneted!")
+    })
+})
+
+//  ---------> Run expressServer
+expressServer.listen(5000, function(){
+    console.log("@Server run @ 5000!")
 })
 
